1fdd76b39d54437e08dd1304172b51662663e81f
iostef94 <114776052+iostef94@users.noreply.github.com>
2025-10-03 12:10:59 +0300

    updates

--- DeloitteIntegration/DeloitteIntegration/Program.cs
+++ DeloitteIntegration/DeloitteIntegration/Program.cs
@@ -1,23 +1,33 @@
+using DeloitteIntegration.Infrastructure.DependencyInjection;
+using DeloitteIntegration.Application.Interfaces;
+using DeloitteIntegration.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Application layer
+builder.Services.AddScoped<ICityService, CityService>();
+
+// Add Infrastructure layer
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
